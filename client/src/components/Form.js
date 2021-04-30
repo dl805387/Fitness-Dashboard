081efc0f1fb52970e52de2f3afb45b70ab8ec73d
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-04-29 21:45:33 -0400

    implemented dynamic input fields

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -6,10 +6,32 @@ function Form(props) {
     const {user_id} = props;
 
     const [text, setText] = useState({});
+
+    const nani = (name, value) => {
+        //setText({first: "1"});
+        // let f = text;
+        // let obj = f;
+        // obj["second"] = "2";
+        // return obj;
+
+        let obj = {};
+        obj = {...text};
+        //obj = ...text;
+        obj[name] = value;
+        return obj;
+    }
+
+    useEffect(() => {
+        //setText({first: "1"});
+        // let f = {};
+        // let obj = f;
+        // obj["second"] = "2";
+        // console.log(obj);
+    }, []);
    
 
     const Input = () => {
-        return <input type="text" onChange={e => setText({ [e.target.name]: e.target.value })}></input>;
+        return <input type="text" onChange={e => setText(nani(e.target.name, e.target.value))}></input>;
     };
     const [inputList, setInputList] = useState([]);
 
@@ -25,7 +47,8 @@ function Form(props) {
             {inputList}
             <br>
             </br>
-            <input type="text" name="llll" onChange={e => setText({ [e.target.name]: e.target.value })}></input>
+            <input type="text" name="first" onChange={e => setText(nani(e.target.name, e.target.value))}></input>
+            <input type="text" name="second" onChange={e => setText(nani(e.target.name, e.target.value))}></input>
             <input type="text" name="pppp" onChange={e => setText({ [e.target.name]: e.target.value })}></input>
             {console.log(text)}
             
